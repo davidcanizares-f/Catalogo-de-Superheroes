7eb848ad96c5ed6118655004b904f2cc5b053dd2
David Canizares <scaniizares@gmail.com>
2026-05-19 19:17:55 -0500

    Etiqueta inicial

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+=============== UNIVERSIDAD DE LAS AMÉRICAS ===============
+        FACULTAD DE INGENIERÍA Y CIENCIAS APLICADAS
+                INGENIERÍA DE SOFTWARE
+                    PROGRAMACIÓN I
+Autores:
+    -David Cañizares
+    -Sebastián Orbe
+    -Jean Pierre Rosas
+    -Derek Yépez
+
+================= CATÁLOGO DE SUPERHÉROES =================
+
+
+
+*/
